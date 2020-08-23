26f4e3b73138e62798ebf588c5c9de57470b18bc
markponi <markjponiatowski@gmail.com>
2020-08-23 10:10:49 -0400

    updates from menu bar

---
+++ app/src/main/java/com/mjponi/tasktimer/AppProvider.java
@@ -0,0 +1,270 @@
+package com.mjponi.tasktimer;
+
+import android.content.ContentProvider;
+import android.content.ContentValues;
+import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+import android.net.Uri;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+/**
+ * Provider for the TaskTimer app. This is the only class that knows about {@link AppDatabase}
+ *
+ */
+
+public class AppProvider extends ContentProvider {
+    private static final String TAG = "AppProvider";
+    private AppDatabase mOpenHelper;
+
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    static final String CONTENT_AUTHORITY = "com.mjponi.tasktimer.provider";
+    public static final Uri CONTENT_AUTHORITY_URI = Uri.parse("content://" + CONTENT_AUTHORITY);
+
+    private static final int TASKS = 100;
+    private static final int TASKS_ID = 101;
+    private static final int TIMINGS = 200;
+    private static final int TIMINGS_ID = 201;
+
+    /*
+    private static final int TASK_TIMINGS = 300;
+    private static final int TASK_TIMINGS_ID = 301;
+     */
+
+    private static final int TASK_DURATIONS = 400;
+    private static final int TASK_DURATIONS_ID = 301;
+
+    private static UriMatcher buildUriMatcher(){
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+
+        // e.g. content://com.mjponi.tasktimer.provider/Tasks
+        matcher.addURI(CONTENT_AUTHORITY, TasksContract.TABLE_NAME, TASKS);
+        //e.g. content://com.mjponi.tasktimer.provider/Tasks/8
+        matcher.addURI(CONTENT_AUTHORITY, TasksContract.TABLE_NAME + "/#", TASKS_ID);
+
+//        matcher.addURI(CONTENT_AUTHORITY, TimingsContract.TABLE_NAME, TIMINGS);
+//        matcher.addURI(CONTENT_AUTHORITY, TimingsContract.TABLE_NAME + "/#", TIMINGS_ID);
+//
+//        matcher.addURI(CONTENT_AUTHORITY, DurationsContract.TABLE_NAME, TASK_DURATIONS);
+//        matcher.addURI(CONTENT_AUTHORITY, DurationsContract.TABLE_NAME + "/#", TASK_DURATIONS_ID);
+
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = AppDatabase.getInstance(getContext());
+        return true;
+    }
+
+    @Nullable
+    @Override
+    public Cursor query(@NonNull Uri uri, @Nullable String[] projection, @Nullable String selection, @Nullable String[] selectionArgs, @Nullable String sortOrder) {
+        Log.d(TAG, "query: called with URI " + uri);
+        final int match = sUriMatcher.match(uri);
+        Log.d(TAG, "query: match is " + match);
+
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+
+        switch (match){
+            case TASKS:
+                queryBuilder.setTables(TasksContract.TABLE_NAME);
+                break;
+
+            case TASKS_ID:
+                queryBuilder.setTables(TasksContract.TABLE_NAME);
+                long taskId = TasksContract.getTaskId(uri);
+                queryBuilder.appendWhere(TasksContract.Columns._ID + " = " + taskId);
+                break;
+
+//            case TIMINGS:
+//                queryBuilder.setTables(TimingsContract.TABLE_NAME);
+//                break;
+
+//            case TIMINGS_ID:
+//                queryBuilder.setTables(TimingsContract.TABLE_NAME);
+//                long timingId = TimingsContract.getTimingId(uri);
+//                queryBuilder.appendWhere(TimingsContract.Columns._ID + " = " + timingId);
+//                break;
+//
+//            case TASK_DURATIONS:
+//                queryBuilder.setTables(DurationsContract.TABLE_NAME);
+//                break;
+//            case TASK_DURATIONS_ID:
+//                queryBuilder.setTables(DurationsContract.TABLE_NAME);
+//                long durationId = DurationsContract.getDurationId(uri);
+//                queryBuilder.appendWhere(DurationsContract.Columns._ID + " = " + durationId);
+//                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown URI: " + uri);
+        }
+
+        SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        return queryBuilder.query(db, projection, selection, selectionArgs, null, null, sortOrder);
+
+    }
+
+    @Nullable
+    @Override
+    public String getType(@NonNull Uri uri) {
+        final int match = sUriMatcher.match(uri);
+        switch (match){
+            case TASKS:
+                return TasksContract.CONTENT_TYPE;
+
+            case TASKS_ID:
+                return TasksContract.CONTENT_ITEM_TYPE;
+
+            default:
+                throw new IllegalArgumentException("unknown Uri: " + uri);
+        }
+    }
+
+    @Nullable
+    @Override
+    public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
+        Log.d(TAG, "Entering insert, called with uri: " + uri);
+        final int match = sUriMatcher.match(uri);
+        Log.d(TAG, "match is " + match);
+
+        final SQLiteDatabase db;
+
+        Uri returnUri;
+        long recordId;
+        switch (match){
+            case TASKS:
+                db = mOpenHelper.getWritableDatabase();
+                recordId = db.insert(TasksContract.TABLE_NAME, null, values);
+                if(recordId >= 0){
+                    returnUri = TasksContract.buildTaskUri(recordId);
+                } else {
+                    throw new android.database.SQLException("Failed to inert into " + uri.toString());
+                }
+                break;
+            case TIMINGS:
+//                db = mOpenHelper.getWritableDatabase();
+//                recordId = db.insert(TimingsContract.TABLE_NAME, null, values);
+//                if(recordId >= 0){
+//                    returnUri = TimingsContract.Timings.buildTimingUri(recordId);
+//                } else {
+//                    throw new android.database.SQLException("Failed to inert into " + uri.toString());
+//                }
+//                break;
+
+            default:
+                throw new IllegalArgumentException("Unknown uri: " + uri);
+        }
+        Log.d(TAG, "Exiting insert, returning " + returnUri);
+        return returnUri;
+    }
+
+    @Override
+    public int delete(@NonNull Uri uri, @Nullable String selection, @Nullable String[] selectionArgs) {
+        Log.d(TAG, "delete called with Uri " + uri);
+        final int match = sUriMatcher.match(uri);
+        Log.d(TAG, "match is " + match);
+
+        final SQLiteDatabase db;
+        int count;
+
+        String selectionCriteria;
+
+        switch (match){
+            case TASKS:
+                db = mOpenHelper.getWritableDatabase();
+                count = db.delete(TasksContract.TABLE_NAME, selection, selectionArgs);
+                break;
+
+            case TASKS_ID:
+                db = mOpenHelper.getWritableDatabase();
+                long taskId = TasksContract.getTaskId(uri);
+                selectionCriteria = TasksContract.Columns._ID + " = " + taskId;
+
+                if((selection != null) && (selection.length() >0)){
+                    selectionCriteria += "AND (" + selection + ")";
+                }
+                count = db.delete(TasksContract.TABLE_NAME, selectionCriteria, selectionArgs);
+                break;
+
+//            case TIMINGS:
+//                db = mOpenHelper.getWritableDatabase();
+//                count = db.delete(TimingsContract.TABLE_NAME, selection, selectionArgs);
+//                break;
+//
+//            case TIMINGS_ID:
+//                db = mOpenHelper.getWritableDatabase();
+//                long timingsId = TimingsContract.getTimingsId(uri);
+//                selectionCriteria = TimingsContract.Columns._ID + " = " + timingsId;
+//
+//                if((selection != null) && (selection.length() >0)){
+//                    selectionCriteria += "AND (" + selection + ")";
+//                }
+//                count = db.delete(TimingsContract.TABLE_NAME, selectionCriteria, selectionArgs);
+//                break;
+
+
+            default:
+                throw new IllegalArgumentException("Unknown uri: " + uri);
+        }
+        Log.d(TAG, "Exiting update, returning " + count);
+        return count;
+    }
+
+    @Override
+    public int update(@NonNull Uri uri, @Nullable ContentValues values, @Nullable String selection, @Nullable String[] selectionArgs) {
+        Log.d(TAG, "update called with Uri " + uri);
+        final int match = sUriMatcher.match(uri);
+        Log.d(TAG, "match is " + match);
+
+        final SQLiteDatabase db;
+        int count;
+
+        String selectionCriteria;
+
+        switch (match){
+            case TASKS:
+                db = mOpenHelper.getWritableDatabase();
+                count = db.update(TasksContract.TABLE_NAME, values, selection, selectionArgs);
+                break;
+
+            case TASKS_ID:
+                db = mOpenHelper.getWritableDatabase();
+                long taskId = TasksContract.getTaskId(uri);
+                selectionCriteria = TasksContract.Columns._ID + " = " + taskId;
+
+                if((selection != null) && (selection.length() >0)){
+                    selectionCriteria += "AND (" + selection + ")";
+                }
+                count = db.update(TasksContract.TABLE_NAME, values, selectionCriteria, selectionArgs);
+                break;
+
+//            case TIMINGS:
+//                db = mOpenHelper.getWritableDatabase();
+//                count = db.update(TimingsContract.TABLE_NAME, values, selection, selectionArgs);
+//                break;
+//
+//            case TIMINGS_ID:
+//                db = mOpenHelper.getWritableDatabase();
+//                long timingsId = TimingsContract.getTimingsId(uri);
+//                selectionCriteria = TimingsContract.Columns._ID + " = " + timingsId;
+//
+//                if((selection != null) && (selection.length() >0)){
+//                    selectionCriteria += "AND (" + selection + ")";
+//                }
+//                count = db.update(TimingsContract.TABLE_NAME, values, selectionCriteria, selectionArgs);
+//                break;
+
+
+            default:
+                throw new IllegalArgumentException("Unknown uri: " + uri);
+        }
+        Log.d(TAG, "Exiting update, returning " + count);
+        return count;
+    }
+}
--- app/src/main/java/com/mjponi/tasktimer/MainActivity.java
+++ app/src/main/java/com/mjponi/tasktimer/MainActivity.java
@@ -1,5 +1,8 @@
 package com.mjponi.tasktimer;
 
+import android.content.ContentResolver;
+import android.content.ContentValues;
+import android.database.Cursor;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
@@ -8,12 +11,14 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.util.Log;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +26,65 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        String[] projection = { TasksContract.Columns._ID,
+                                TasksContract.Columns.TASKS_NAME,
+                                TasksContract.Columns.TASKS_DESCRIPTION,
+                                TasksContract.Columns.TASKS_SORTORDER};
+
+        ContentResolver contentResolver = getContentResolver();
+
+        ContentValues values = new ContentValues();
+
+//        values.put(TasksContract.Columns.TASKS_SORTORDER, "99");
+//        values.put(TasksContract.Columns.TASKS_DESCRIPTION, "Completed");
+//        String selection = TasksContract.Columns.TASKS_SORTORDER + " = " + 2;
+//
+//        int count = contentResolver.update(TasksContract.CONTENT_URI, values, selection, null);
+//        Log.d(TAG, "onCreate: " + count + " record(s) updated.");
+
+//        values.put(TasksContract.Columns.TASKS_DESCRIPTION, "For deletion");
+//        String selection = TasksContract.Columns.TASKS_SORTORDER + " = ?";
+//        String[] args = {"99"};
+//
+//        int count = contentResolver.delete(TasksContract.buildTaskUri(3), null, null);
+        String selection = TasksContract.Columns.TASKS_DESCRIPTION + " = ? ";
+        String[] args = { "For deletion"};
+        int count = contentResolver.delete(TasksContract.CONTENT_URI, selection, args);
+        Log.d(TAG, "onCreate: " + count + " record(s) deleted.");
+
+
+//        values.put(TasksContract.Columns.TASKS_NAME, "Content Provider");
+//        values.put(TasksContract.Columns.TASKS_DESCRIPTION, "Record Content Provider video");
+//        int count = contentResolver.update(TasksContract.buildTaskUri(4), values, null, null);
+//        Log.d(TAG, "onCreate: " + count + " records(s) updated");
+
+//        values.put(TasksContract.Columns.TASKS_NAME, "New Task 1");
+//        values.put(TasksContract.Columns.TASKS_DESCRIPTION, "Description 1");
+//        values.put(TasksContract.Columns.TASKS_SORTORDER, 2);
+//        Uri uri = contentResolver.insert(TasksContract.CONTENT_URI, values);
+
+
+        Cursor cursor = contentResolver.query(TasksContract.CONTENT_URI,
+//        Cursor cursor = contentResolver.query(TasksContract.buildTaskUri(2),
+                projection,
+                null,
+                null,
+                TasksContract.Columns.TASKS_SORTORDER);
+
+        if(cursor != null){
+            Log.d(TAG, "onCreate: number of rows: " + cursor.getCount());
+            while (cursor.moveToNext()){
+                for(int i=0;i<cursor.getColumnCount();i++){
+                    Log.d(TAG, "onCreate: " + cursor.getColumnName(i) + ": " + cursor.getString(i));
+                }
+                Log.d(TAG, "onCreate: =================================");
+            }
+            cursor.close();
+        }
+
+//        AppDatabase appDatabase = AppDatabase.getInstance(this);
+//        final SQLiteDatabase db = appDatabase.getReadableDatabase();
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -47,7 +111,7 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.menumain_settings) {
             return true;
         }
 
